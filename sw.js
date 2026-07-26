cb321a697f5b396e034f23d7cefe7f50681a5b75
yikezhong1 <yikezhong@qq.com>
2026-07-26 23:40:56 +0800

    deploy sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'pom-kb-202607262159';
+var CACHE_NAME = 'pom-kb-202607262339';
 var CACHE_URLS = ['index.html', 'marked.min.js', 'purify.min.js', 'data.json'];
 
 self.addEventListener('install', function(event) {
